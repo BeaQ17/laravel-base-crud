35f19245da7443bb375af6f0886107f26a62ed43
BeaQ17 <beaq235@gmail.com>
2021-07-18 23:46:53 +0200

    update form

--- base-crud/resources/views/create.blade.php
+++ base-crud/resources/views/create.blade.php
@@ -7,31 +7,31 @@
 <form action="" method="post">
     <div class="form-group">
         <label for="">Title</label>
-        <input type="text" placeholder="Add title" id="title">
+        <input type="text" placeholder="Add title" id="title" name="title">
     </div>
     <div>
         <label for="">Cover image</label>
-        <input type="text" placeholder="Add cover image as an url" id="cover">
+        <input type="text" placeholder="Add cover image as an url" id="cover" name="cover">
     </div>
     <div>
         <label for="">Issue number</label>
-        <input type="text" placeholder="Add the issue number" id="issue_number">
+        <input type="text" placeholder="Add the issue number" id="issue_number" name="issue_number">
     </div>
     <div>
         <label for="">Year</label>
-        <input type="text" placeholder="Add publication year" id="year">
+        <input type="text" placeholder="Add publication year" id="year" name="year">
     </div>
     <div>
         <label for="">Publisher</label>
-        <input type="text" placeholder="Add publishing house" id="publisher">
+        <input type="text" placeholder="Add publishing house" id="publisher" name="publisher">
     </div>
     <div>
         <label for="">Writer</label>
-        <input type="text" placeholder="Add writer" id="writer">
+        <input type="text" placeholder="Add writer" id="writer" name="writer">
     </div>
     <div>
         <label for="">Colorist</label>
-        <input type="text" placeholder="Add colorist" id="colorist">
+        <input type="text" placeholder="Add colorist" id="colorist" name="colorist">
     </div>
     <div>
         <button type="submit">Add</button>
