dde180793f1f21569ed02b55b643797b703b8cc6
BeaQ17 <beaq235@gmail.com>
2021-07-18 22:18:47 +0200

    update index + show

--- base-crud/resources/views/index.blade.php
+++ base-crud/resources/views/index.blade.php
@@ -7,6 +7,29 @@
         <title>Issues</title>
     </head>
     <body>
-       <h1>Hello</h1>
+       
+
+        @section('content')
+
+            <div class="card">
+                <section class="container">
+
+                    @foreach ($issues as $issue)
+                        <div class="issue">
+                            <h1>{{$issue->title}}</h1>
+                            <h2>{{$issue->issue_number}}</h2>
+                            <img src="{{$issue->cover}}" alt="issue cover">
+                            <p>{{$issue->publisher}} - {{$issue->year}}</p>
+                            <p>Writer: {{$issue->writer}} - Colorist: {{$issue->colorist}}</p>
+                            <hr>
+                        </div>
+                        
+                    @endforeach
+
+                </section>
+            </div>
+            
+        @endsection
+
     </body>
 </html>
--- base-crud/resources/views/show.blade.php
+++ base-crud/resources/views/show.blade.php
@@ -1,20 +1,18 @@
 @section('content')
 
-<div class="card">
-    <section class="container">
+    <div class="card">
+        <section class="container">
 
-        @foreach ($issues as $issue)
             <div class="issue">
                 <h1>{{$issue->title}}</h1>
                 <h2>{{$issue->issue_number}}</h2>
+                <img src="{{$issue->cover}}" alt="issue cover">
                 <p>{{$issue->publisher}} - {{$issue->year}}</p>
                 <p>Writer: {{$issue->writer}} - Colorist: {{$issue->colorist}}</p>
                 <hr>
             </div>
-            
-        @endforeach
 
-    </section>
-</div>
-    
-@endsection
+        </section>
+    </div>
+            
+ @endsection
